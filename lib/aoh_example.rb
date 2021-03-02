cb349623b545894c241a55d248fb0cb17071f950
Paul H Graham Jr <pjhg14@gmail.com>
2021-03-02 15:29:37 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -1,4 +1,3 @@
-require "pry"
 # Examples inspired by David Foster Wallace's # (2/21/1962 to 9/12/2008) _Infinite Jest_
 
 DON_G = { name:  "Don Gately", occupation: "Live-in Staff" }
@@ -33,7 +32,6 @@ end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
-  binding.pry
   aoh[row][key] = new_value
 
   aoh
